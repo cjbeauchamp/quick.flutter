835a0d79a519aa6d1b4587b907c0e06bdc275536
Chris Beauchamp <cjbeauchamp@gmail.com>
2024-03-29 08:58:31 -0400

    mac support fixes

--- packages/quick_usb/lib/src/quick_usb_desktop.dart
+++ packages/quick_usb/lib/src/quick_usb_desktop.dart
@@ -21,10 +21,12 @@ class QuickUsbWindows extends _QuickUsbDesktop {
 }
 
 class QuickUsbMacos extends _QuickUsbDesktop {
-  // For example/.dart_tool/flutter_build/generated_main.dart
-  static registerWith() {
-    QuickUsbPlatform.instance = QuickUsbMacos();
-    _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+  QuickUsbMacos() {
+    if(Platform.version.contains('arm64')) {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+    } else {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.0.dylib'));
+    }
   }
 }
 
